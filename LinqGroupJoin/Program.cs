0ef881967d0cdbff725405bd704a9f1c4720cb87
Ali Avşar <vimsagrup@VimsaGrup.onmicrosoft.com>
2024-11-28 00:47:31 +0300

    odev

---
+++ LinqGroupJoin/Program.cs
@@ -0,0 +1,43 @@
+﻿using LinqGroupJoin;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Student> students = new List<Student>();
+        students.Add(new Student { studentId = 1 , studentName = "Ali" , sclassId = 1});
+        students.Add(new Student { studentId = 2, studentName = "Ayşe", sclassId = 2 });
+        students.Add(new Student { studentId = 3, studentName = "Mehmet", sclassId = 1 });
+        students.Add(new Student { studentId = 4, studentName = "Fatma", sclassId = 3 });
+        students.Add(new Student { studentId = 5, studentName = "Ahmet", sclassId = 2 });
+
+        List<Sinif> sinifs = new List<Sinif>();
+        sinifs.Add(new Sinif { classId = 1, className = "Matematik" });
+        sinifs.Add(new Sinif { classId = 1, className = "Türkçe" });
+        sinifs.Add(new Sinif { classId = 1, className = "Kimya" });
+
+        var studentssinifs = students.GroupJoin(sinifs,
+                                                student => student.sclassId,
+                                                sinif => sinif.classId,
+                                                (student, studentsinif) => new
+                                                {
+                                                    studentname = student.studentName,
+                                                    sinifs = studentsinif
+                                                }
+                                                );
+
+        foreach (var student in studentssinifs)
+        {
+            Console.WriteLine("-------------");
+            Console.WriteLine($"{student.studentname}");
+            Console.WriteLine("----> Dersler");
+            foreach (var sinif in student.sinifs)
+            {
+                Console.WriteLine($"{sinif.className}");
+            }
+        }
+
+
+
+    }
+}
---
+++ LinqGroupJoin/students.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqGroupJoin
+{
+    public class Student
+    {
+        public int studentId { get; set; }
+        public string studentName { get; set; }
+        public int sclassId { get; set; }
+
+    }
+
+    public class Sinif
+    {
+        public int classId { get; set; }
+        public string className { get; set; }
+    }
+}
